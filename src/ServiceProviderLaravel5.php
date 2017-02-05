fe262e9c771b7dcd777525bf4358443149755397
Dylan Akhawais <dylanakhawais@live.co.uk>
2017-02-05 01:32:34 +0000

    Fixes

--- src/ServiceProviderLaravel5.php
+++ src/ServiceProviderLaravel5.php
@@ -27,16 +27,16 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
         $this->mergeConfigFrom(__DIR__.'/config/config.php', 'slack');
 
         $this->app['maknz.slack'] = new Client(
-                $app['config']->get('slack.endpoint'),
+                $this->app['config']->get('slack.endpoint'),
                 [
-                    'channel' => $app['config']->get('slack.channel'),
-                    'username' => $app['config']->get('slack.username'),
-                    'icon' => $app['config']->get('slack.icon'),
-                    'link_names' => $app['config']->get('slack.link_names'),
-                    'unfurl_links' => $app['config']->get('slack.unfurl_links'),
-                    'unfurl_media' => $app['config']->get('slack.unfurl_media'),
-                    'allow_markdown' => $app['config']->get('slack.allow_markdown'),
-                    'markdown_in_attachments' => $app['config']->get('slack.markdown_in_attachments'),
+                    'channel' => $this->app['config']->get('slack.channel'),
+                    'username' => $this->app['config']->get('slack.username'),
+                    'icon' => $this->app['config']->get('slack.icon'),
+                    'link_names' => $this->app['config']->get('slack.link_names'),
+                    'unfurl_links' => $this->app['config']->get('slack.unfurl_links'),
+                    'unfurl_media' => $this->app['config']->get('slack.unfurl_media'),
+                    'allow_markdown' => $this->app['config']->get('slack.allow_markdown'),
+                    'markdown_in_attachments' => $this->app['config']->get('slack.markdown_in_attachments'),
                 ],
                 new Guzzle
             );
